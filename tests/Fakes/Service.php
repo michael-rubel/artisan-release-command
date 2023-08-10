b0d12cd0a20fb289c83ee841aff08ffd210b17f5
Michael Rubél <michael@laravel.software>
2023-08-10 13:37:44 +0200

    `pint` fix.

--- tests/Fakes/Service.php
+++ tests/Fakes/Service.php
@@ -6,6 +6,6 @@ namespace MichaelRubel\ArtisanRelease\Tests\Fakes;
 
 class Service
 {
-    final public const VERSION = '0.0.2';
+    final public const VERSION     = '0.0.2';
     final public const APP_VERSION = '0.0.2';
 }
